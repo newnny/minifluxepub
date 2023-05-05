94e7f9a93d6a06b79500239a48d42209a498701a
Yuni <newnny0812@gmail.com>
2023-05-05 22:27:11 +0200

    fixed problem with backend function

--- api/download.ts
+++ api/download.ts
@@ -11,11 +11,6 @@ module.exports = (request: VercelRequest, response: VercelResponse) => {
 import type { VercelRequest, VercelResponse } from '@vercel/node';
 
 export default function (request: VercelRequest, response: VercelResponse) {
-  response.setHeader('Access-Control-Allow-Origin', '*');
-  response.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS, PATCH');
-  response.setHeader('Access-Control-Allow-Headers', 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version');
-  response.setHeader('Access-Control-Allow-Credentials', 'true');
-
   const { name = 'World' } = request.query;
   response.send(`Hello ${name}!`);
 
