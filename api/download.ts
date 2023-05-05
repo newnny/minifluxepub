583c42c7f669c80016890c68bdb28cb2beb2ea87
Yuni <newnny0812@gmail.com>
2023-05-05 09:55:49 +0200

    error test

--- api/download.ts
+++ api/download.ts
@@ -1,4 +1,5 @@
 module.exports = (request, response) => {
     response.setHeader('content-type','text/plain');
+    console.error("there has been an error, this is just a test")
     response.send('hello world, this is download endpoint')
 }
