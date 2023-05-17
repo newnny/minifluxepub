6ac1ada33d249af20dbb007cbe9388208de5838d
Yuni <newnny0812@gmail.com>
2023-05-17 16:30:18 +0200

    loading page

---
+++ src/utils/Loading.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './Loading.css';
+
+const Loading: React.FC = () => {
+  return (
+    <div id="load">
+      <div>G</div>
+      <div>N</div>
+      <div>I</div>
+      <div>D</div>
+      <div>A</div>
+      <div>O</div>
+      <div>L</div>
+    </div>
+  )
+}
+
+export default Loading
