0003aea5aa802b6aa590a764e1caf3151a1be3d8
Yuni <newnny0812@gmail.com>
2023-05-19 12:41:43 +0200

    modifying css

--- src/components/SinglePageMain.tsx
+++ src/components/SinglePageMain.tsx
@@ -1,31 +1,36 @@
-import React, {useRef} from 'react';
+import React, { useRef, useEffect } from 'react';
 
 import Main from './Main';
 import Introduction from './Introduction';
 import Start from './Start';
 
 const SinglePageMain: React.FC = () => {
-    const introductionRef = useRef<HTMLDivElement>(null);
-    const startRef = useRef<HTMLDivElement>(null);
-  
-    const scrollToIntroduction = () => {
-      introductionRef.current?.scrollIntoView({ behavior: 'smooth' })
-    }
-  
-    const scrollToStart = () => {
-      startRef.current?.scrollIntoView({ behavior: 'smooth' })
-    }
-    return (
-        <div>
-            <Main scrollToIntroduction={scrollToIntroduction} scrollToStart={scrollToStart} />
-          <div id="introduction" ref={introductionRef}>
-            <Introduction />
-          </div>
-          <div id="start" ref={startRef}>
-            <Start />
-          </div>
-        </div>
-    )
+  const introductionRef = useRef<HTMLDivElement>(null);
+  const startRef = useRef<HTMLDivElement>(null);
+
+  const scrollToIntroduction = () => {
+    introductionRef.current?.scrollIntoView({ behavior: 'smooth' })
+  }
+
+  const scrollToStart = () => {
+    startRef.current?.scrollIntoView({ behavior: 'smooth' })
+  }
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
+
+  return (
+    <div>
+      <Main scrollToIntroduction={scrollToIntroduction} scrollToStart={scrollToStart} />
+      <div id="introduction" ref={introductionRef}>
+        <Introduction />
+      </div>
+      <div id="start" ref={startRef}>
+        <Start />
+      </div>
+    </div>
+  )
 }
 
 export default SinglePageMain;
