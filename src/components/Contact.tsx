467da55fb71a772ac763e7c21a8c115c1a12b890
Yuni <newnny0812@gmail.com>
2023-06-23 16:10:21 +0200

    modifications

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,18 +1,40 @@
 
 import SendIcon from '../icons/send-svgrepo-com.svg'
+import Giveicon from '../icons/network-svgrepo-com.svg'
+
 const Contact: React.FC = () => {
   return (
     <div className='Landing-div-wrapper'>
-      <p style={{ fontSize: 18 }}>
-        We kindly request you to share your valuable opinions or any inquiries with us here.
-      </p>
-
-      <a className="a-no-deco" href="mailto:newnny0812@gmail.com" target="_blank" rel="noreferrer">
-        <img
-          src={SendIcon}
-          className='contact-send-icon'
-        />
-      </a>
+      <div className='contact-div'>
+        <div style={{ padding: 30 }}>
+          <p style={{ fontSize: 18, height: 80 }}>
+            Please take a moment to share your valuable opinions of 'Minifluxbinder' with us.<br />
+            Clicking the icon below will redirect you to our survey page
+          </p>
+          <a className="a-no-deco" href="https://www.surveymonkey.de/r/LSLCJPQ" target="_blank" rel="noreferrer">
+            <img
+              src={Giveicon}
+              className='contact-send-icon'
+            />
+          </a>
+        </div>
+        <div style={{ padding: 30 }}>
+          <p style={{ fontSize: 18, height: 80 }}>
+            Beyond the survey, if you have any inquiries, we kindly ask you to share them with us by email.
+          </p>
+          <a className="a-no-deco" href="mailto:newnny0812@gmail.com" target="_blank" rel="noreferrer">
+            <img
+              src={SendIcon}
+              className='contact-send-icon'
+            />
+          </a>
+        </div>
+      </div>
+      <div>
+        <p style={{ fontSize: 18, paddingTop: 30 }}>
+          Thank you for being a part of our journey towards service excellence!
+        </p>
+      </div>
     </div>
   )
 }
